93ae034a197b451ac8580ce454e183b62c9a62da
unknown <fogaca135@gmail.com>
2021-01-04 12:46:11 -0300

    Project created

---
+++ backend/src/main/java/com/br/corporatepilot/flightlogbook/FlightlogbookApplication.java
@@ -0,0 +1,13 @@
+package com.br.corporatepilot.flightlogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightlogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightlogbookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/br/corporatepilot/flightlogbook/FlightlogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.corporatepilot.flightlogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightlogbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
