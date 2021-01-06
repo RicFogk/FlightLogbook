56bcf082d3c9573bb935237b586f44510e854da2
unknown <fogaca135@gmail.com>
2021-01-06 16:31:23 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,17 @@
 // NAO PRECISA MAIS IMPORTAR -->>   import React from 'react';
 import React from 'react';
 import './App.css';
-import Hello from './Hello';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div>
+    
+    //fragment
+    <>
       <Navbar/>
-    </div>
+      <Home/>
+    </>
   );
 }
 
