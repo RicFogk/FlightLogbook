9017a860d2b1900b3a082e1cd3ccb33259c911c4
unknown <fogaca135@gmail.com>
2021-01-06 14:27:01 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+// NAO PRECISA MAIS IMPORTAR -->>   import React from 'react';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
